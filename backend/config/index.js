bd793a449d9056dfa0c49c89daffd495449b204d
Parknx14 <parknx14@gmail.com>
2026-05-27 12:30:34 +0530

    update22

--- backend/config/index.js
+++ backend/config/index.js
@@ -61,7 +61,7 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRES_IN: '7d',
 
-  BASE_URL: process.env.BASE_URL || 'http://localhost:5000',
+  BASE_URL: process.env.BASE_URL || 'https://url-shortner-8e04.onrender.com/',
 
   RATE_LIMIT: {
     windowMs: 15 * 60 * 1000,
