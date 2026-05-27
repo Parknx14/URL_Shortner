8496cd681208fa653cd5280b0b59632112394e48
Parknx14 <parknx14@gmail.com>
2026-05-27 12:40:45 +0530

    99

--- backend/server.js
+++ backend/server.js
@@ -243,7 +243,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://url-shortner-j3iivye9d-parknx14.vercel.app'
+    'https://url-shortner-git-main-parknx14.vercel.app/'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
