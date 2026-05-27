8ee235cc86ef16290a9cd01ec0faefb9a8fdd8fd
Parknx14 <parknx14@gmail.com>
2026-05-27 12:37:56 +0530

    update - 001

--- backend/server.js
+++ backend/server.js
@@ -240,11 +240,15 @@ const urlRoutes  = require('./routes/urls');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://url-shortner-one-virid.vercel.app/'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://url-shortner-j3iivye9d-parknx14.vercel.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
-
 app.use(express.json());
 app.use('/api', generalLimiter);
 app.use('/api/auth', authRoutes);
